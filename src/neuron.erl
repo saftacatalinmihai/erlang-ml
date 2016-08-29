642f10054b206068b2feb07ca846e4303cfd7eec
mihai <saftacatalinmihai@gmail.com>
2016-08-29 18:07:14 +0300

    added debugging function that sets the weights of a neuron

--- src/neuron.erl
+++ src/neuron.erl
@@ -10,7 +10,7 @@
   inputs = [],
   output_Pids = []}).
 
-%% API
+%% API -----------------------------------------------------------------------------------------------------------------
 start_link([W, I, OP]) -> gen_server:start_link(?MODULE, [W, I, OP], []).
 
 stimulate(Pid, Inputs) -> gen_server:cast(Pid, {stimulate, Inputs}).
@@ -21,35 +21,22 @@ connect(Sender_PID, Receiver_PID) ->
 
 pass(Pid, Input) -> gen_server:cast(Pid, {pass, Input}).
 
-%%------------
+%% Debugging api
+set_weights(Pid, W) -> gen_server:call(Pid, {set_weights, W }).
+
+%%----------------------------------------------------------------------------------------------------------------------
 
 hyp(W, A) -> ml_math:hyp(W, A, fun ml_math:sigmoid/1).
 
 perceive(Inp, Weights) ->
     hyp([1|Inp], Weights).
 
-%% Hard coded some vals, should be fine...
-rand_weights(Number) -> rand_weights(Number, 0.12).
-rand_weights(Number, Epsilon_init) ->
-    [rand:uniform() * 2 * Epsilon_init - Epsilon_init || _ <- lists:seq(1, Number)].
-
-back_prop_delta(output, Actual, Predicted) ->
-     Predicted - Actual.
-
-back_prop_delta(hidden, Z, WeightsOut, DeltaNextLayer) ->
-    ml_math:hyp(WeightsOut, DeltaNextLayer) * ml_math:sigmoid_grad(Z).
-
-back_prop_deriv(Activation, DeltaNextLayer) -> Activation * DeltaNextLayer.
-
 init([W, I, OP]) ->
   {ok, #state{weights = W, inputs = I, output_Pids = OP }}.
-
 
 handle_cast({stimulate, Input}, #state{weights=Weights, inputs=Inputs, output_Pids = Output_PIDs}) ->
   New_inputs = replace_input(Inputs, Input),
   Output = perceive(convert_to_list(New_inputs), Weights),
-
-%%  io:format("Output is: ~p~n", [Output]),
 
   if Output_PIDs =/= [] ->
     lists:foreach(fun(Output_PID) ->
@@ -67,9 +54,9 @@ handle_cast({connect_to_output, Receiver_PID}, State) ->
   {noreply, State#state{output_Pids = Combined_output}};
 
 handle_cast({connect_to_input, Sender_PID}, #state{weights=Weights, inputs=Inputs, output_Pids = Output_PIDs}) ->
-  Combined_input = [{Sender_PID, 0.5} | Inputs],
+  Combined_input = [{Sender_PID, 0} | Inputs],
   io:format("~w inputs connected to ~w: ~w~n", [self(), Sender_PID, Combined_input]),
-  {noreply, #state{weights = [0.5 | Weights], inputs = Combined_input, output_Pids = Output_PIDs}};
+  {noreply, #state{weights = [rand:uniform() | Weights], inputs = Combined_input, output_Pids = Output_PIDs}};
 
 handle_cast({pass, Input_value}, State) ->
   lists:foreach(fun(Output_PID) ->
@@ -77,15 +64,21 @@ handle_cast({pass, Input_value}, State) ->
     neuron:stimulate(Output_PID, {self(), Input_value})
                 end,
     State#state.output_Pids),
-  {noreply, State}.
+  {noreply, State};
 
+handle_cast(_,_) ->
+  erlang:error(not_implemented).
+
+handle_call({set_weights, W }, _, State) ->
+  {reply, ok, State#state{weights = W}};
 
 handle_call(_, _, _) ->
   erlang:error(not_implemented).
 
 handle_info(Other, State) ->
   io:format("Unexpected message: ~p~n",[Other]),
-  io:format("State was: ~p~n",[State]).
+  io:format("State was: ~p~n",[State]),
+  {noreply,State}.
 
 terminate(Reason, State) ->
   io:format("Terminating for reason: ~p~n State was ~p~n",[Reason, State]),
@@ -93,7 +86,6 @@ terminate(Reason, State) ->
 
 code_change(_OldVsn, State, _Extra) ->
   {ok, State}.
-
 
 replace_input(Inputs, Input) ->
   {Input_PID, _} = Input,
