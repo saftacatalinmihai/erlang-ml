ee3a22521cb1c4eebf169a1d86801ff84bd4bc25
mihai <saftacatalinmihai@gmail.com>
2016-08-29 17:49:35 +0300

    made neuron a gen_server

--- src/neuron.erl
+++ src/neuron.erl
@@ -1,6 +1,27 @@
 -module(neuron).
 
--export([hyp/2, perceive/2, rand_weights/1]).
+%%-export([hyp/2, perceive/2, rand_weights/1]).
+-compile(export_all).
+
+-behavior(gen_server).
+
+-record(state, {
+  weights = [],
+  inputs = [],
+  output_Pids = []}).
+
+%% API
+start_link([W, I, OP]) -> gen_server:start_link(?MODULE, [W, I, OP], []).
+
+stimulate(Pid, Inputs) -> gen_server:cast(Pid, {stimulate, Inputs}).
+
+connect(Sender_PID, Receiver_PID) ->
+  gen_server:cast(Sender_PID, {connect_to_output, Receiver_PID}),
+  gen_server:cast(Receiver_PID, {connect_to_input, Sender_PID}).
+
+pass(Pid, Input) -> gen_server:cast(Pid, {pass, Input}).
+
+%%------------
 
 hyp(W, A) -> ml_math:hyp(W, A, fun ml_math:sigmoid/1).
 
@@ -10,13 +31,77 @@ perceive(Inp, Weights) ->
 %% Hard coded some vals, should be fine...
 rand_weights(Number) -> rand_weights(Number, 0.12).
 rand_weights(Number, Epsilon_init) ->
-    [rand:uniform(100)/ 100 * 2 * Epsilon_init - Epsilon_init || _ <- lists:seq(1, Number)].
+    [rand:uniform() * 2 * Epsilon_init - Epsilon_init || _ <- lists:seq(1, Number)].
 
 back_prop_delta(output, Actual, Predicted) ->
-     Predicted - Actual;
+     Predicted - Actual.
 
-back_prop_delta(hidden, Activation, WeightsOut, DeltaNextLayer) ->
-    ml_math:hyp(WeightsOut, DeltaNextLayer).                      %% * ml_math:sigmoid_grad(Activation).
+back_prop_delta(hidden, Z, WeightsOut, DeltaNextLayer) ->
+    ml_math:hyp(WeightsOut, DeltaNextLayer) * ml_math:sigmoid_grad(Z).
+
+back_prop_deriv(Activation, DeltaNextLayer) -> Activation * DeltaNextLayer.
+
+init([W, I, OP]) ->
+  {ok, #state{weights = W, inputs = I, output_Pids = OP }}.
 
 
-back_prop_deriv(Activation, DeltaNextLayer) = Activation * DeltaNextLayer
+handle_cast({stimulate, Input}, #state{weights=Weights, inputs=Inputs, output_Pids = Output_PIDs}) ->
+  New_inputs = replace_input(Inputs, Input),
+  Output = perceive(convert_to_list(New_inputs), Weights),
+
+%%  io:format("Output is: ~p~n", [Output]),
+
+  if Output_PIDs =/= [] ->
+    lists:foreach(fun(Output_PID) ->
+      neuron:stimulate(Output_PID, {self(), Output})
+                  end,
+      Output_PIDs);
+    Output_PIDs =:= [] ->
+      io:format("~n~w outputs: ~w", [self(), Output])
+  end,
+  {noreply, #state{weights = Weights, inputs = New_inputs, output_Pids = Output_PIDs}};
+
+handle_cast({connect_to_output, Receiver_PID}, State) ->
+  Combined_output = [Receiver_PID | State#state.output_Pids],
+  io:format("~w output connected to ~w: ~w~n", [self(), Receiver_PID, Combined_output]),
+  {noreply, State#state{output_Pids = Combined_output}};
+
+handle_cast({connect_to_input, Sender_PID}, #state{weights=Weights, inputs=Inputs, output_Pids = Output_PIDs}) ->
+  Combined_input = [{Sender_PID, 0.5} | Inputs],
+  io:format("~w inputs connected to ~w: ~w~n", [self(), Sender_PID, Combined_input]),
+  {noreply, #state{weights = Weights, inputs = Combined_input, output_Pids = Output_PIDs}};
+
+handle_cast({pass, Input_value}, State) ->
+  lists:foreach(fun(Output_PID) ->
+    io:format("Stimulating ~w with ~w~n", [Output_PID, Input_value]),
+    neuron:stimulate(Output_PID, {self(), Input_value})
+                end,
+    State#state.output_Pids),
+  {noreply, State}.
+
+
+handle_call(_, _, _) ->
+  erlang:error(not_implemented).
+
+handle_info(Other, State) ->
+  io:format("Unexpected message: ~p~n",[Other]),
+  io:format("State was: ~p~n",[State]).
+
+terminate(Reason, State) ->
+  io:format("Terminating for reason: ~p~n State was ~p~n",[Reason, State]),
+  ok.
+
+code_change(_OldVsn, State, _Extra) ->
+  {ok, State}.
+
+
+replace_input(Inputs, Input) ->
+  {Input_PID, _} = Input,
+  lists:keyreplace(Input_PID, 1, Inputs, Input).
+
+convert_to_list(Inputs) ->
+  lists:map(fun(Tup) ->
+    {_, Val} = Tup,
+    Val
+            end,
+    Inputs).
--- test/neuron_tests.erl
+++ test/neuron_tests.erl
@@ -19,3 +19,41 @@ perceive_test_() ->
             neuron:perceive([1,1], [-30,20,20]) > 0.9999
         end
     ].
+
+neuron_gen_server_test_() ->
+  {ok, Pid} = neuron:start_link([[0.01, 0.5, 0.2], [{1,0.6}, {2,0.9}], []]),
+  L = [fun () ->
+    ok = neuron:stimulate(Pid, {1, 0.3}),
+    ok = neuron:stimulate(Pid, {1, 0.4}),
+    ok = neuron:stimulate(Pid, {2, 0.5})
+  end],
+  {ok, N1_pid} = neuron:start_link([[0],[],[]]),
+  {ok, N2_pid} = neuron:start_link([[0],[],[]]),
+  {ok, N3_pid} = neuron:start_link([[0],[],[]]),
+  neuron:connect(N1_pid, N2_pid),
+  neuron:connect(N1_pid, N3_pid),
+
+  L ++ [
+    fun () ->
+      neuron:pass(N1_pid, 0.5)
+    end
+  ].
+
+xor_test_() ->
+  {ok, Input1} = neuron:start_link([[0],[],[]]),
+  {ok, Input2} = neuron:start_link([[0],[],[]]),
+  {ok, N1} = neuron:start_link([[-30, 20, 20],[],[]]),
+  {ok, N2} = neuron:start_link([[30, -20, -20],[],[]]),
+  {ok, N3} = neuron:start_link([[-10, 20, 20],[],[]]),
+
+  neuron:connect(Input1, N1),
+  neuron:connect(Input2, N1),
+  neuron:connect(Input1, N2),
+  neuron:connect(Input2, N2),
+  neuron:connect(N1, N3),
+  neuron:connect(N2, N3),
+
+  fun () ->
+    neuron:pass(Input1, 0),
+    neuron:pass(Input2, 0)
+  end.
