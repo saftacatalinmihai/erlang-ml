e8ef6497cba5e95eea198fe4a10baf8d1d45de9b
mihai <saftacatalinmihai@gmail.com>
2016-08-31 14:40:54 +0300

    refactoring to records

--- src/neuron.erl
+++ src/neuron.erl
@@ -5,23 +5,31 @@
 
 -behavior(gen_server).
 
--record(state, {
-  weights = [],
-  input_nodes = [],
-  output_nodes = [],
-  output_value = 0
-  }).
+-record( input, {
+  weight      = rand:uniform(), %% the weight associated to the input stimulus
+  activation  = 0               %% the last activation of input node
+}).
+
+-record( output, {
+  backprop = 0                  %% backprop = weight * delta
+}).
+
+-record( state, {
+  inputs     = #{1 => #input{activation = 1}},  %% map between input Node and record. Starts with the bias term id = 1
+  outputs    = #{},                             %% map between output Node and record
+  activation = 0                                %% last activation of this node
+}).
 
 %% API -----------------------------------------------------------------------------------------------------------------
-start_link([W, I, OP]) -> gen_server:start_link(?MODULE, [W, I, OP], []).
+start_link() -> gen_server:start_link(?MODULE, [], []).
 
-stimulate(Node, Inputs) -> gen_server:cast(Node, {stimulate, Inputs}).
+stimulate(Node, FromNode, Activation) -> gen_server:cast(Node, {stimulate, FromNode, Activation}).
 
-learn(NodeBehind, Node, Delta) -> gen_server:cast(NodeBehind, {learn, {Node, Delta}}).
+%%learn(BackNode, Node, Delta) -> gen_server:cast(BackNode, {learn, {Node, Delta}}).
 
-connect(Sender_PID, Receiver_PID) ->
-  gen_server:cast(Sender_PID, {connect_to_output, Receiver_PID}),
-  gen_server:cast(Receiver_PID, {connect_to_input, Sender_PID}).
+connect(Input_node, Output_Node) ->
+  gen_server:cast(Input_node, {connect_to_output, Output_Node}),
+  gen_server:cast(Output_Node, {connect_to_input, Input_node}).
 
 pass(Pid, Input) -> gen_server:cast(Pid, {pass, Input}).
 
@@ -35,86 +43,119 @@ get_output(Pid) -> gen_server:call(Pid, get_output).
 hyp(W, A) -> ml_math:hyp(W, A, fun ml_math:sigmoid/1).
 
 perceive(Inp, Weights) ->
-    hyp([1|Inp], Weights).
+    hyp(Inp, Weights).
 
-init([W, I, OP]) ->
-  {ok, #state{weights = W, input_nodes = I, output_nodes = OP }}.
+init([]) ->
+  {ok, #state{}}.
 
-handle_cast({stimulate, Input}, #state{weights=Weights, input_nodes =Inputs, output_nodes = Output_nodes}) ->
-  New_inputs = replace_input(Inputs, Input),
-  Output = perceive(convert_to_list(New_inputs), Weights),
+handle_cast({stimulate, FromNode, Activation}, State) ->
+  #{FromNode := InputNode} = State#state.inputs,
 
-  if Output_nodes =/= [] ->
-    lists:foreach(fun(Output_PID) ->
-      neuron:stimulate(Output_PID, {self(), Output})
-                  end,
-      convert_to_keys(Output_nodes));
-    Output_nodes =:= [] ->
-      io:format("~n~w outputs: ~w", [self(), Output]),
-      neuron:learn(self(), self(), 1)
+  New_Inputs = maps:update(
+    FromNode,
+    InputNode#input{activation = Activation},
+    State#state.inputs
+  ),
 
+  InputList = maps:values(New_Inputs),
+  Neuron_Activation = perceive(
+    lists:map(fun(I) -> I#input.activation end, InputList),
+    lists:map(fun(I) -> I#input.weight     end, InputList)
+  ),
+
+  case maps:size(State#state.outputs) of
+    0 ->
+      io:format("~n~w Activation: ~w", [self(), Neuron_Activation]);
+%%      neuron:learn(self(), self(), 1)
+    _ ->
+      lists:foreach(
+        fun(Output_PID) ->
+          neuron:stimulate(Output_PID, self(), Neuron_Activation)
+        end,
+        maps:keys(State#state.outputs)
+      )
   end,
-  {noreply, #state{weights = Weights, input_nodes = New_inputs, output_nodes = Output_nodes, output_value = Output}};
+  {noreply, State#state{inputs = New_Inputs, activation = Neuron_Activation}};
+%%
+%%handle_cast({learn, Backprop}, #state{weights=Weights, input_nodes = Inputs, output_nodes = Outputs}) ->
+%%
+%%  Learning_rate = 0.5,
+%%
+%%  % Calculate the correct sensitivities
+%%  io:format("Outputs: ~p~n", [Outputs]),
+%%  io:format("Backprop: ~p~n", [Backprop]),
+%%  New_outputs = add_delta(Outputs, Backprop),
+%%  Output_value = perceive(convert_to_list(Inputs), Weights),
+%%  Derv_value = ml_math:hyp(Weights, [1|convert_to_values(Inputs)], fun ml_math:sigmoid_deriv/1),
+%%  Delta = calculate_delta(Backprop, Inputs, New_outputs,
+%%    Output_value, Derv_value),
+%%  io:format("(~w) New Sensitivities: ~w~n", [self(), New_outputs]),
+%%  io:format("(~w) Calculated Sensitivity: ~w~n", [self(), Delta]),
+%%
+%%  % Adjust all the weights
+%%  Weight_adjustments = lists:map(fun(Input) ->
+%%    Learning_rate * Delta * Input
+%%                                 end,
+%%    convert_to_values(Inputs)),
+%%  New_weights = lists:zipwith(fun(W, D) -> W + D end, tl(Weights), Weight_adjustments),
+%%  io:format("(~w) Adjusted Weights: ~w~n", [self(), Weights]),
+%%
+%%  % propagate sensitivities and associated weights back to the previous layer
+%%%%  lists:foreach(
+%%%%    fun ({Weight, Input_PID}) ->
+%%%%      neuron:learn(Input_PID, self(), Delta * Weight)
+%%%%    end,
+%%%%    lists:zip(New_weights, convert_to_keys(Inputs)),
+%%%%  ),
+%%%%  {noreply,  #state{weights = Weights, input_nodes = Inputs, output_nodes = Outputs, output_value = Output_value}};
+%%  {noreply, #state{weights = New_weights, input_nodes = Inputs, output_nodes = New_outputs, activation = Output_value}};
 
-handle_cast({learn, Backprop}, #state{weights=Weights, input_nodes = Inputs, output_nodes = Outputs}) ->
+handle_cast({connect_to_output, Output_node}, State) ->
+  NewState = State#state{
+    outputs = maps:put(Output_node, #output{}, State#state.outputs)
+  },
+  io:format("~w output connected to ~w: ~w~n", [self(), Output_node, maps:keys(NewState#state.outputs)]),
+  {noreply, NewState};
 
-  Learning_rate = 0.5,
-
-  % Calculate the correct sensitivities
-  io:format("~p~n", [Outputs]),
-  io:format("~p~n", [Backprop]),
-  New_outputs = add_delta(Outputs, Backprop),
-  Output_value = perceive(convert_to_list(Inputs), Weights),
-  Derv_value = ml_math:hyp(Weights, [1|convert_to_values(Inputs)], fun ml_math:sigmoid_deriv/1),
-  Delta = calculate_delta(Backprop, Inputs, New_outputs,
-    Output_value, Derv_value),
-  io:format("(~w) New Sensitivities: ~w~n", [self(), New_outputs]),
-  io:format("(~w) Calculated Sensitivity: ~w~n", [self(), Delta]),
-
-  % Adjust all the weights
-  Weight_adjustments = lists:map(fun(Input) ->
-    Learning_rate * Delta * Input
-                                 end,
-    convert_to_values(Inputs)),
-  New_weights = lists:zipwith(fun(W, D) -> W + D end, tl(Weights), Weight_adjustments),
-  io:format("(~w) Adjusted Weights: ~w~n", [self(), Weights]),
-
-  % propagate sensitivities and associated weights back to the previous layer
-%%  lists:foreach(
-%%    fun ({Weight, Input_PID}) ->
-%%      neuron:learn(Input_PID, self(), Delta * Weight)
-%%    end,
-%%    lists:zip(New_weights, convert_to_keys(Inputs)),
-%%  ),
-%%  {noreply,  #state{weights = Weights, input_nodes = Inputs, output_nodes = Outputs, output_value = Output_value}};
-  {noreply, #state{weights = New_weights, input_nodes = Inputs, output_nodes = New_outputs, output_value = Output_value}};
-
-handle_cast({connect_to_output, Receiver_PID}, State) ->
-  Combined_output = [{Receiver_PID,1} | State#state.output_nodes],
-  io:format("~w output connected to ~w: ~w~n", [self(), Receiver_PID, Combined_output]),
-  {noreply, State#state{output_nodes = Combined_output}};
-
-handle_cast({connect_to_input, Sender_PID}, #state{weights=Weights, input_nodes =Inputs, output_nodes = Output_PIDs}) ->
-  Combined_input = [{Sender_PID, 0}| Inputs],
-  io:format("~w inputs connected to ~w: ~w~n", [self(), Sender_PID, Combined_input]),
-  {noreply, #state{weights = [rand:uniform() | Weights], input_nodes = Combined_input, output_nodes = Output_PIDs}};
+handle_cast({connect_to_input, Input_node}, State) ->
+  NewState = State#state{
+    inputs = maps:put(Input_node, #input{}, State#state.inputs)
+  },
+  io:format("~w inputs connected to ~w: ~w~n", [self(), Input_node, maps:keys(NewState#state.inputs)]),
+  {noreply, NewState};
 
 handle_cast({pass, Input_value}, State) ->
-  lists:foreach(fun(Output_PID) ->
-    io:format("Stimulating ~w with ~w~n", [Output_PID, Input_value]),
-    neuron:stimulate(Output_PID, {self(), Input_value})
-                end,
-    convert_to_keys(State#state.output_nodes)),
-  {noreply, State#state{output_value =Input_value}};
+  lists:foreach(
+    fun(Output_Node) ->
+      io:format("Stimulating ~w with ~w~n", [Output_Node, Input_value]),
+      neuron:stimulate(Output_Node, self(), Input_value)
+    end,
+    maps:keys(State#state.outputs)),
+
+  {noreply, State#state{activation =Input_value}};
 
 handle_cast(_,_) ->
   erlang:error(not_implemented).
 
-handle_call({set_weights, W }, _, State) ->
-  {reply, ok, State#state{weights = W}};
+handle_call({set_weights, W}, _, State) ->
+  New_State = lists:foldl(
+    fun({Id, Weight}, S) ->
+      Input = maps:get(Id, S#state.inputs),
+      S#state{
+        inputs = maps:update(
+          Id,
+          Input#input{weight = Weight},
+          S#state.inputs
+        )
+      }
+    end,
+    State,
+    W
+  ),
+  {reply, ok, New_State};
 
 handle_call(get_output, _, State) ->
-  {reply, State#state.output_value, State};
+  {reply, State#state.activation, State};
 
 handle_call(_, _, _) ->
   erlang:error(not_implemented).
@@ -131,45 +172,44 @@ terminate(Reason, State) ->
 code_change(_OldVsn, State, _Extra) ->
   {ok, State}.
 
-replace_input(Inputs, Input) ->
-  {Input_PID, _} = Input,
-  lists:keyreplace(Input_PID, 1, Inputs, Input).
+%%replace_input(Inputs, Input) ->
+%%  lists:keyreplace(Input#input.node, 1, Inputs, Input).
 
-% adds the propagating sensitivity to the Sensitivities Hash
-add_delta(Deltas, Backprop) when Deltas =/= [] ->
-  replace_input(Deltas, Backprop);
-add_delta(Deltas, _Backprop) when Deltas =:= [] ->
-  [].
-
-% Calculates the sensitivity of this particular node
-calculate_delta(_, Inputs, Outputs, _, _)
-  when Outputs =/= [], Inputs =:= [] -> % When the node is an input node:
-  null;
-calculate_delta(Delta, Inputs, Sensitivities, Output_value, Derv_value)
-  when Sensitivities =:= [], Inputs =/= [] -> % When the node is an output node:
-  {_, Training_value} = Delta,
-  (Training_value - Output_value) * Derv_value;
-calculate_delta(_, Inputs, Outputs, _, Derv_value)
-  when Outputs =/= [], Inputs =/= [] -> % When the node is a hidden node:
-  Derv_value * lists:foldl(fun(E, T) -> E + T end, 0, convert_to_values(Outputs)).
-
-convert_to_list(Inputs) ->
-  lists:map(fun(Tup) ->
-    {_, Val} = Tup,
-    Val
-            end,
-    Inputs).
-
-convert_to_values(Tuple_list) ->
-  lists:map(fun(Tup) ->
-    {_, Val} = Tup,
-    Val
-            end,
-    Tuple_list).
-
-convert_to_keys(Tuple_list) ->
-  lists:map(fun(Tup) ->
-    {Key, _} = Tup,
-    Key
-            end,
-    Tuple_list).
+%%% adds the propagating sensitivity to the Sensitivities Hash
+%%add_delta(Deltas, Backprop) when Deltas =/= [] ->
+%%  replace_input(Deltas, Backprop);
+%%add_delta(Deltas, _Backprop) when Deltas =:= [] ->
+%%  [].
+%%
+%%% Calculates the sensitivity of this particular node
+%%calculate_delta(_, Inputs, Outputs, _, _)
+%%  when Outputs =/= [], Inputs =:= [] -> % When the node is an input node:
+%%  null;
+%%calculate_delta(Delta, Inputs, Sensitivities, Output_value, Derv_value)
+%%  when Sensitivities =:= [], Inputs =/= [] -> % When the node is an output node:
+%%  {_, Training_value} = Delta,
+%%  (Training_value - Output_value) * Derv_value;
+%%calculate_delta(_, Inputs, Outputs, _, Derv_value)
+%%  when Outputs =/= [], Inputs =/= [] -> % When the node is a hidden node:
+%%  Derv_value * lists:foldl(fun(E, T) -> E + T end, 0, convert_to_values(Outputs)).
+%%
+%%convert_to_list(Inputs) ->
+%%  lists:map(fun(Tup) ->
+%%    {_, Val} = Tup,
+%%    Val
+%%            end,
+%%    Inputs).
+%%
+%%convert_to_values(Tuple_list) ->
+%%  lists:map(fun(Tup) ->
+%%    {_, Val} = Tup,
+%%    Val
+%%            end,
+%%    Tuple_list).
+%%
+%%convert_to_keys(Tuple_list) ->
+%%  lists:map(fun(Tup) ->
+%%    {Key, _} = Tup,
+%%    Key
+%%            end,
+%%    Tuple_list).
--- test/neuron_tests.erl
+++ test/neuron_tests.erl
@@ -4,48 +4,44 @@
 
 perceive_test_() ->
     [   fun () ->
-            neuron:perceive([neuron:perceive([1,1], [-10, 20, 20]), neuron:perceive([1,1], [-30, 20, 20])], [10, 20, -20]) > 0.9999
+            neuron:perceive([1, neuron:perceive([1,1,1], [-10, 20, 20]), neuron:perceive([1,1,1], [-30, 20, 20])], [10, 20, -20]) > 0.9999
         end,
         fun () ->
-            neuron:perceive([0,0], [-30,20,20]) < 0.0001
+            neuron:perceive([1,0,0], [-30,20,20]) < 0.0001
         end,
         fun () ->
-            neuron:perceive([0,1], [-30,20,20]) < 0.0001
+            neuron:perceive([1,0,1], [-30,20,20]) < 0.0001
         end,
         fun () ->
-            neuron:perceive([1,0], [-30,20,20]) < 0.0001
+            neuron:perceive([1,1,0], [-30,20,20]) < 0.0001
         end,
         fun () ->
-            neuron:perceive([1,1], [-30,20,20]) > 0.9999
+            neuron:perceive([1,1,1], [-30,20,20]) > 0.9999
         end
     ].
 
 neuron_gen_server_test_() ->
-  {ok, Pid} = neuron:start_link([[0.01, 0.5, 0.2], [{1,0.6}, {2,0.9}], []]),
-  L = [fun () ->
-    ok = neuron:stimulate(Pid, {1, 0.3}),
-    ok = neuron:stimulate(Pid, {1, 0.4}),
-    ok = neuron:stimulate(Pid, {2, 0.5})
-  end],
-  {ok, N1_pid} = neuron:start_link([[0],[],[]]),
-  {ok, N2_pid} = neuron:start_link([[0],[],[]]),
-  {ok, N3_pid} = neuron:start_link([[0],[],[]]),
-  neuron:connect(N1_pid, N2_pid),
-  neuron:connect(N1_pid, N3_pid),
+  {ok, N1} = neuron:start_link(),
+  {ok, N2} = neuron:start_link(),
+  {ok, N3} = neuron:start_link(),
+  {ok, N4} = neuron:start_link(),
 
-  L ++ [
+  neuron:connect(N1, N2),
+  neuron:connect(N1, N3),
+  neuron:connect(N2, N4),
+  neuron:connect(N3, N4),
+  [
     fun () ->
-      neuron:pass(N1_pid, 0.5)
+      neuron:pass(N1, 0.5)
     end
   ].
 
 xor_test_() ->
-  {ok, Input1} = neuron:start_link([[0],[],[]]),
-  {ok, Input2} = neuron:start_link([[0],[],[]]),
-
-  {ok, N1} = neuron:start_link([[],[],[]]),
-  {ok, N2} = neuron:start_link([[],[],[]]),
-  {ok, N3} = neuron:start_link([[],[],[]]),
+  {ok, Input1} = neuron:start_link(),
+  {ok, Input2} = neuron:start_link(),
+  {ok, N1} = neuron:start_link(),
+  {ok, N2} = neuron:start_link(),
+  {ok, N3} = neuron:start_link(),
 
   neuron:connect(Input1, N1),
   neuron:connect(Input2, N1),
@@ -54,9 +50,9 @@ xor_test_() ->
   neuron:connect(N1, N3),
   neuron:connect(N2, N3),
 
-  neuron:set_weights(N1, [-10, 20, 20]),
-  neuron:set_weights(N2, [-30, 20, 20]),
-  neuron:set_weights(N3, [10, 20, -20]),
+  neuron:set_weights(N1, [{1, -30}, {Input1, 20}, {Input2, 20}]),
+  neuron:set_weights(N2, [{1,  10}, {Input1,-20}, {Input2,-20}]),
+  neuron:set_weights(N3, [{1, -10}, {N1,     20}, {N2,     20}]),
 
   [
     fun () ->
